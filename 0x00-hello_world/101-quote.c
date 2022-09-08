e742584c99eefb31568b9fad2dca78079891ca45
Pietrez <phaukongo@hotmail.com>
2022-09-08 16:45:02 -0700

    print Dora Korpar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ *  main - Our entry point
+ *  Return: 1
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
