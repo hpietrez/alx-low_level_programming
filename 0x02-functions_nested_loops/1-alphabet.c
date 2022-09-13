a38b7dc59c93dbe5a0046e995a162b28d67a834c
Pietrez <phaukongo@hotmail.com>
2022-09-13 16:30:38 -0700

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: print the alphabet in lowercase
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+}
