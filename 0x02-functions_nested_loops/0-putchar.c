eb9c89668c3ee5e647b22d4e6660469e37e9e3cb
Pietrez <phaukongo@hotmail.com>
2022-09-13 15:29:10 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: print _putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 0;
+	char c [] = "_putchar"
+
+	while (a < 8)
+	{
+		_putchar(c[a]);
+		a++
+	}
+	_putchar('\n');
+	return (0);
+}
