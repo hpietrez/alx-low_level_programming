0ff1a70ca9838242acd3bb37c5904bd395eb8580
Pietrez <phaukongo@hotmail.com>
2022-09-13 16:43:17 -0700

    alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ * Description: print the alphabet ten times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+
+	for (a = 0; a < 10; i++)
+	{
+		char alp;
+
+		for (alp = 'a'; alp <='z'; alp++)
+		{
+			_putchar(alp);
+		}
+		_putchar('\n');
+	}
+}
