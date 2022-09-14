3ba08f9cf6ccc3e951ab1d1ec1eb0093c17b9126
Pietrez <phaukongo@hotmail.com>
2022-09-13 18:31:12 -0700

    jack bauer 24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - entry point
+ * Description: print every minute of the day from 0:00 to 23:59
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
