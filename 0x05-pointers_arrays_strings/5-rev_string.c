0f2e899ba46951cb73325ae18f728b47f76804eb
Pietrez <phaukongo@hotmail.com>
2022-09-19 18:18:01 -0700

    rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -17,3 +17,19 @@ void rev_string(char *s);
 		*(s + len - i - 1) = tmp;
 	}
 }
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
