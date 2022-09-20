571eb398bac26ac6395a8acb4db38dc7ef1c54f2
Pietrez <phaukongo@hotmail.com>
2022-09-19 18:50:38 -0700

    rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,24 +6,21 @@
  * Return: void
  */
 
-void rev_string(char *s);
+void rev_string(char *s)
 {
-	int i, j, k, temp;
+	int i = 0, j = 0;
+	char str[500];
 
-	i- 0;
-	while (s[i] != '\0')
+	while (*(s + i))
 	{
+		*(str + i) = *(s + i);
 		i++;
 	}
-	k = 0;
-	j = i - 1;
-
-	while (k < j)
+	i = i - 1;
+	while (i >= 0)
 	{
-		temp = s[k];
-		s[k] = s[j];
-		s[j] = temp;
-		k++;
-		j--;
+		*(s + i) = *(str + j);
+		j++;
+		i--;
 	}
 }
